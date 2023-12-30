0adfc67c7af2f2f2acb6e356c9156aabe23b4415
MetrisVailore <122095030+MetrisVailore@users.noreply.github.com>
2023-12-30 19:37:29 +0100

    Update kolka.js

--- src/kolka.js
+++ src/kolka.js
@@ -5,12 +5,12 @@ https://github.com/revoxhere/duino-coin/blob/useful-tools
 
 const poolRewards = require("../config/poolRewards.json");
 const highestPCdiff = poolRewards["NET"]["difficulty"] * 5.19202;
-const highestESPdiff = poolRewards["ESP32"]["difficulty"];
+const highestESPdiff = poolRewards["DUE"]["difficulty"];
 const highestAVRdiff = poolRewards["DUE"]["difficulty"];
 const gpuMiningPercentage =
-    poolRewards["EXTREME"]["kolka_decrease_perc"] * 0.1;
+    poolRewards["NET"]["kolka_decrease_perc"] * 0.1;
 const pcMiningPercentage = poolRewards["NET"]["kolka_decrease_perc"] * 0.001;
-const espMiningPercentage = poolRewards["ESP32"]["kolka_decrease_perc"] * 0.001;
+const espMiningPercentage = poolRewards["AVR"]["kolka_decrease_perc"] * 0.001;
 const avrMiningPercentage = poolRewards["AVR"]["kolka_decrease_perc"] * 0.001;
 
 function V1(hashrate, difficulty, workers, reward_div) {
